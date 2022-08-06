5adae99fbf06a451f892f070e92f0108928ecda1
Rahul K (433154) <rahul.kumar-8@alstomgroup.com>
2022-08-06 22:14:33 +0530

    model

--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,13 @@
 //using selectors inside the element
 // traversing the dom
+
+const minusBtn = document.querySelector('.minus-icon');
+const plusBtn = document.querySelectorAll('.plus-icon');
+const btns = document.querySelectorAll('.question-btn');
+console.log(btns);
+
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    console.log(e.currentTarget.parentElement.parentElement);
+  });
+});
